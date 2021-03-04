7ab3ebf3438d3d7f0a47364dcf82b5cc667defb3
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-03-04 08:49:44 +0800

    ready to integrate apis

--- src/containers/Faucet/index copy.js
+++ src/containers/Faucet/index copy.js
@@ -1,16 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from "react";
 import Arweave from "arweave";
+import Twitter from "twitter-v2";
+import { TwitterClient } from 'twitter-api-client';
 import fileDownload from "js-file-download";
 import { Col, Container, Row } from "react-bootstrap";
 import { FaucetContainer } from "./style";
 import { Button, Input } from "antd";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+const config = {
+  consumer_key: 'EKhbBpQ4i0Fvnp7I1emd8QI7V',
+  consumer_secret: 'msjd3WDiDjxR5iJU7TM32tW75f2P3fXgzHBuFJMdadnXbm8qtg',
+  access_token_key: '268566493-sTwrL0d5GwqmxTjF13TIhS2QfeIsGawmB2jbkwJZ',
+  access_token_secret: 'GY2tn8c4L56wpVfdkCJjkiejiy8Zt4o8SISBNtzsDNfVS'
+}
 
 function Faucet() {
+  const token =
+    "AAAAAAAAAAAAAAAAAAAAABZDNQEAAAAAJGt4FImlProy%2FwlT%2F1PpejL%2FcYg%3DPqn5NDHiuQwyPMr5CuO3njcUkTWuwkJ6Ol64l1njVmatGq5sXg";
+    const twitterClient = new TwitterClient({
+      apiKey: 'rvGVlRVVOzrVJ8Gr5HuMIIl6n',
+      apiSecret: 'rvzedsxEFUwlQCjYLQRvF4ayzmGU3hwU62COUbkVoZIotQfAXV',
+      accessToken: '<YOUR-TWITTER-ACCESS-TOKEN>',
+      accessTokenSecret: '<YOUR-TWITTER-ACCESS-TOKEN-SECRET>',
+    });
+  const twitter_client = new Twitter({
+    // consumer_key: "rvGVlRVVOzrVJ8Gr5HuMIIl6n",
+    // consumer_secret: "rvzedsxEFUwlQCjYLQRvF4ayzmGU3hwU62COUbkVoZIotQfAXV",
+    bearer_token: token,
+  });
   const history = useHistory();
   console.log({ history });
-  const [address, setAddress] = useState(null);
+  const [address, setAddress] = useState('HjgZrnSWuloWdMQXu_9ne13Vy-34VAWNtO3FAM8rch8');
 
   const onDownloadFile = async () => {
     const arweave = Arweave.init({
@@ -32,9 +54,48 @@ function Faucet() {
   };
 
   const enterButtonHandler = async () => {
-    
+    // const res = await axios({
+    //   method: "GET",
+    //   url: "https://api.twitter.com/1.1/search/tweets.json",
+    //   params: {
+    //     q: '"' + address + '"',
+    //   },
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //     "Access-Control-Allow-Origin": "*",
+    //   },
+    // });
+
+    const data = await twitterClient.tweets.search({ q: address });
+    console.log({data})
+
+    // const params = {
+    //   q: address
+    // }
+    // twitter_client.get('search/tweets', params, async function(err, data, response) {
+
+    //   console.log({ err, data, response });
+    // })
+    // const submission = {
+    //   targetAddress: address,
+    //   qty: 50,
+    // };
+    // _api(submission);
   };
-  
+  const _api = () => {
+    // const url = process.env.REACT_APP_API_URL;
+    // axios
+    //   .post(url, submission)
+    //   .then((res) => {
+    //     console.log(`statusCode: ${res.statusCode}`);
+    //     console.log(res);
+    //     setState({ ...state, showModal: true });
+    //   })
+    //   .catch((error) => {
+    //     //   console.error(error)
+    //   });
+  };
+  console.log({ address });
   return (
     <FaucetContainer>
       <Container>
@@ -72,11 +133,15 @@ function Faucet() {
           <h2>Your Key: {address}</h2>
           <Button
             onClick={tweetButtonHandler}
-            className='btn-blueDark'
+            variant="contained"
             disabled={!address}
           >
             <a
-              href={`https://twitter.com/intent/tweet?text=I%27m%20verifying%20my%20Koi%20address%20${address}&via=open_koi`}
+              href={
+                address
+                  ? `https://twitter.com/intent/tweet?text=I%27m%20verifying%20my%20Koi%20address%20${address}&via=open_koi`
+                  : "#/"
+              }
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -84,7 +149,8 @@ function Faucet() {
             </a>
           </Button>
           <Button
-            className='btn-blueDark'
+            variant="contained"
+            color="primary"
             onClick={enterButtonHandler}
           >
             Get Koi
