04147c0e73f0671a018e9719891b696dd34d30a7
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-03-04 23:57:55 +0800

    code clean

--- src/containers/Faucet/index.js
+++ src/containers/Faucet/index.js
@@ -8,11 +8,10 @@ import { Button, Input } from "antd";
 import { useHistory } from "react-router-dom";
 
 function Faucet() {
+
   const history = useHistory();
-  console.log({ history });
   const [address, setAddress] = useState(null);
   const [step, setStep] = useState(0);
-  // const [hasAddress, setHasAddress] = useState(false);
 
   const onSkipGetWallet = () => {
     setStep(1);
@@ -189,55 +188,6 @@ function Faucet() {
             </div>
           </Carousel.Item>
         </Carousel>
-        {/* <div className="section section-set-key">
-          <h2>Set your key</h2>
-          <Row className="set-key-wrapper">
-            <Col>
-              <div className="set-koi-left set-koi-card">
-                <p>If you have a wallet address</p>
-                <Input
-                  type="text"
-                  onChange={(e) => setAddress(e.target.value)}
-                  placeholder="input your adress"
-                />
-                <p>OR</p>
-                <Button>Upload your file</Button>
-              </div>
-            </Col>
-            <Col>
-              <div className="set-koi-right set-koi-card">
-                <p>or don't have a wallet? Please download your key file.</p>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={onDownloadFile}
-                  disabled={false}
-                >
-                  DOWNLOAD ARWEAVE WALLET
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </div>
-        <div className="section section-get-koi">
-          <h2>Your Key: {address}</h2>
-          <Button
-            onClick={tweetButtonHandler}
-            className="btn-blueDark"
-            disabled={!address}
-          >
-            <a
-              href={`https://twitter.com/intent/tweet?text=I%27m%20verifying%20my%20Koi%20address%20${address}&via=open_koi`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Open Tweet Pop-up
-            </a>
-          </Button>
-          <Button className="btn-blueDark" onClick={enterButtonHandler}>
-            Get Koi
-          </Button>
-        </div> */}
       </Container>
     </FaucetContainer>
   );
