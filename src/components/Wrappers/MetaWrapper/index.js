4436c25a5d2a4408a4e75254ebe02068bae6dd64
eagle <eagle930705@gmail.com>
2021-04-08 20:10:15 -0700

    update meta data

--- src/components/Wrappers/MetaWrapper/index.js
+++ src/components/Wrappers/MetaWrapper/index.js
@@ -1,7 +1,12 @@
 import React, {useEffect} from 'react'
 import { Helmet } from 'react-helmet'
 
-const MetaWrapper = ({ title = '', description = '', keywords = '', children }) => {
+const MetaWrapper = ({ 
+  title = '', 
+  description = 'Koi Leaderboard | Earn KOI for attention', 
+  keywords = '', 
+  children 
+}) => {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [children])
@@ -12,16 +17,23 @@ const MetaWrapper = ({ title = '', description = '', keywords = '', children }) 
         <meta name='description' content={description} />
         <meta name='robots' content='index,follow' />
         <meta name='keywords' content={keywords} />
-        <meta property='og:image' content='' />
-        <meta property='og:image:width' content='' />
-        <meta property='og:image:height' content='' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Rentorr' />
-        <meta property='og:description' content='' />
-        <meta property='og:url' content='' />
-        <meta property='og:site_name' content='Rentorr' />
-        <meta name='twitter:card' content='summary' />
-        <meta name='theme-color' content='' />
+        <meta name="theme-color" content="#000000" />
+        <meta name="og:title" content="Koi Leaderboard | Earn KOI for attention" />
+        <meta name="og:description" content="Contribute extra computation, earn your share." />
+        <meta name="og:image" content="https://koi.rocks/koi-360x250.png" />
+        <meta property="og:image" content="https://koi.rocks/koi-360x250.png" />
+        <meta name="twitter:image" content="https://koi.rocks/koi-360x250.png" />
+        <meta name="og:url" content="https://koi.rocks/" />
+        <meta name="og:url" content="https://koi.rocks/" />
+        <meta name="og:type" content="website" />
+        <meta property="og:title" content="Koi Leaderboard | Earn KOI for attention" />
+        <meta property="og:description" content="We're the record for web3. Become a witness today!" />
+        <meta property="og:url" content="https://koi.rocks/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="The Permenant Record | KOI" />
+        <meta name="twitter:description" content="Contribute extra computation, earn your share." />
+        <meta name="twitter:creator" content="" />
       </Helmet>
       <>
         {children}
