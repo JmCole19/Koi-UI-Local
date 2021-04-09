43b28e765ea3cfdf9864e1c83fce268d79994aa9
bluedreampro <bluedreampro2017@gmail.com>
2021-04-09 03:54:47 -0700

    update openGraph validation

--- src/components/Wrappers/MetaWrapper/index.js
+++ src/components/Wrappers/MetaWrapper/index.js
@@ -16,25 +16,25 @@ const MetaWrapper = ({
     <>
       <Helmet>
         <title>{title}</title>
-        <meta name='description' content={description} />
+        <meta name='description' content={description.replace(/['"]+/g, '`')} />
         <meta name='robots' content='index,follow' />
-        <meta name='keywords' content={keywords} />
+        <meta name='keywords' content={keywords.replace(/['"]+/g, '`')} />
         <meta name="theme-color" content="#000000" />
-        <meta name="og:title" content={title} />
-        <meta name="og:description" content={description} />
+        <meta name="og:title" content={title.replace(/['"]+/g, '`')} />
+        <meta name="og:description" content={description.replace(/['"]+/g, '`')} />
         <meta name="og:image" content={imageUrl} />
         <meta property="og:image" content={imageUrl} />
         <meta name="og:url" content={url} />
         <meta name="og:url" content={url} />
         <meta name="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title.replace(/['"]+/g, '`')} />
+        <meta property="og:description" content={description.replace(/['"]+/g, '`')} />
         <meta property="og:url" content={url} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={imageUrl} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
+        <meta name="twitter:title" content={title.replace(/['"]+/g, '`')} />
+        <meta name="twitter:description" content={description.replace(/['"]+/g, '`')} />
         <meta name="twitter:creator" content="" />
       </Helmet>
       <>
